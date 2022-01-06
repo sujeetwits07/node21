8ad37657664e1307a41bd6053bdd4ad27868912c
sujeetwits07 <97097176+sujeetwits07@users.noreply.github.com>
2022-01-06 11:04:41 +0530

    Add files via upload

---
+++ sorting.js
@@ -0,0 +1,14 @@
+/*const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+console.log(fruits);*/
+/*const num = [40, 12, 1000, 5, 25, 604, 20, 10];
+num.sort(function (a, b) {
+  return b - a;
+});
+console.log(num);*/
+
+const num = [40, 12, 1000, 5, 25, 604, 20, 10];
+num.sort(function (a, b) {
+  return a - b;
+});
+console.log(num);
