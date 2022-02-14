486b172a99cd55b33687e20d6dfb531df08d8370
sujeetwits07 <97097176+sujeetwits07@users.noreply.github.com>
2022-02-14 18:40:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../src/pages/login";
+import About from "../src/pages/about";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
