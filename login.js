d908fbcfb37a697e3e445e3ccd81c2336e30d801
sujeetwits07 <97097176+sujeetwits07@users.noreply.github.com>
2022-02-14 18:39:41 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Section, Para } from "../css/login";
+
+const Login = () => {
+  return (
+    <Section>
+      <h1>login</h1>
+      <Para>test</Para>
+    </Section>
+  );
+};
+
+export default Login;
